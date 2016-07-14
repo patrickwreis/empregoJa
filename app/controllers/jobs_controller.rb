ac6e2ca1bf5144b9154f41f2a7eba89063587020
Patrick Reis <patrickwreis@gmail.com>
2016-07-14 13:48:19 -0300

    user create jobs with companies

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,10 +6,11 @@ class JobsController < ApplicationController
 
   def new
     @job = Job.new
+    @companies = Company.all
   end
 
   def create
-    @job = Job.create(params.require(:job).permit(:title, :location, :category, :company, :description, :featured))
+    @job = Job.create(params.require(:job).permit(:title, :location, :category, :company_id, :description, :featured))
     redirect_to @job
   end
 
---
+++ db/migrate/20160714162005_add_company_id_to_jobs.rb
@@ -0,0 +1,5 @@
+class AddCompanyIdToJobs < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :jobs, :company, foreign_key: true
+  end
+end
