df7744e6d1e2c4ba674bf932573fcbdc3eb5691b
Patrick Reis <patrickwreis@gmail.com>
2016-07-17 13:42:04 -0300

    jobs by company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,11 @@
 class CompaniesController < ApplicationController
+
   def show
     @company = Company.find(params[:id])
+    @jobs = @company.jobs
+    if @jobs.empty?
+      flash.now[:notice] = 'Nenhuma vaga disponível'
+    end
   end
 
   def new
